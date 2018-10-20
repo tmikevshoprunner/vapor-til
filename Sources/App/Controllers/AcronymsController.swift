d216c3fda6a6d27916c5e15a642601538d988355
Tyler Mikev <tmikev@shoprunner.com>
2018-10-19 21:26:29 -0500

    Something something uncommited changes

---
+++ Sources/App/Controllers/AcronymsController.swift
@@ -0,0 +1,16 @@
+import Vapor
+import Fluent
+
+struct AcronymsController: RouteCollection {
+    
+    func boot(router: Router) throws {
+        let acronymsRoutes = router.grouped("api", "acronyms")
+
+        acronymsRoutes.get(use: getAllHandler)
+    }
+    
+    func getAllHandler(_ req: Request) throws -> Future<[Acronym]> {
+        return Acronym.query(on: req).all()
+    }
+    
+}
