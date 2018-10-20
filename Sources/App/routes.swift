57a9f96a8f45c0649d37710f3d3b51d9b278339b
Tyler Mikev <tmikev@shoprunner.com>
2018-10-19 21:25:51 -0500

    Use PostgreSQL as the database

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,4 +1,5 @@
 import Vapor
+import Fluent
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
@@ -19,5 +20,51 @@ public func routes(_ router: Router) throws {
             }
     }
     
+    router.get("api", "acronyms", Acronym.parameter) { req -> Future<Acronym> in
+        return try req.parameters.next(Acronym.self)
+    }
     
+    router.put("api", "acronyms", Acronym.parameter) { (req) -> Future<Acronym> in
+        return try flatMap(to: Acronym.self,
+                           req.parameters.next(Acronym.self),
+                           req.content.decode(Acronym.self), {
+                            
+                            (acronym, updatedAcronym) in
+                            acronym.short = updatedAcronym.short
+                            acronym.long = updatedAcronym.long
+                            
+                            return acronym.save(on: req)
+        })
+    }
+    
+    router.delete("api","acronyms", Acronym.parameter) { (req) -> Future<HTTPStatus> in
+        return try req.parameters.next(Acronym.self)
+            .delete(on: req)
+            .transform(to: HTTPStatus.noContent)
+    }
+    
+    router.get("api", "acronyms", "search") { (req) -> Future<[Acronym]> in
+        
+        guard let searchTerm = req.query[String.self, at: "term"] else {
+            throw Abort(.badRequest)
+        }
+        return Acronym.query(on: req).group(.or) { (or) in
+            or.filter(\.short == searchTerm)
+            or.filter(\.long == searchTerm)
+        }.all()
+    }
+    
+    router.get("api", "acronyms", "first") { (req) -> Future<Acronym> in
+        
+        return Acronym.query(on: req).first().map(to: Acronym.self, { (acronym) in
+            guard let acronym = acronym else { throw Abort(.notFound) }
+            
+            return acronym
+        })
+        
+    }
+    
+    let acronymsController = AcronymsController()
+    try router.register(collection: acronymsController)
+
 }
